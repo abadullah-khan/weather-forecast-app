b27c30dd7d4a93f53f115e28e2e999f1f703c68f
Abadullah Khan <abadullahk73@gmail.com>
2023-09-13 21:09:57 +0530

    fixed the header component and made it responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { HiSearch } from "react-icons/hi";
+import { TiWeatherPartlySunny } from "react-icons/ti";
 import style from "../styles/header.module.css";
 import { HandleFetchDataType } from "../types";
 
@@ -10,13 +12,27 @@ const Header = ({ handleFetchData }: Prop) => {
   const [city, setCity] = useState("");
   return (
     <div className={style.header}>
-      <input
-        type="search"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        placeholder="Search..."
-      />
-      <button onClick={() => handleFetchData(city)}>Search</button>
+      <div className={style.logoContainer}>
+        <TiWeatherPartlySunny className={style.logoIcon} />
+        <span>WeatherForecast</span>
+      </div>
+      <div className={style.searchContainer}>
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder="Enter city name"
+          style={{
+            border: ` 2px solid ${
+              city ? "rgb(231, 227, 227)" : "rgb(186, 189, 187)"
+            }`,
+          }}
+        />
+        <button onClick={() => handleFetchData(city)} title="Search weather">
+          <HiSearch className={style.searchIcon} />
+          Search
+        </button>
+      </div>
     </div>
   );
 };
